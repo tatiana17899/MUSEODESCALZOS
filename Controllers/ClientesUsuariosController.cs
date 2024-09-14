0ebc86d92639250a5cd8b63e098d8486a311f538
melivezla <131054190+melivezla@users.noreply.github.com>
2024-09-14 17:20:06 -0500

    Vista cshtml Lista de Usuarios y Clientes + conexión a la base de datos

---
+++ Controllers/ClientesUsuariosController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MUSEODESCALZOS.Data;
+using MuseoDescalzos.Models;
+using System.Threading.Tasks;
+
+namespace MuseoDescalzos.Controllers
+{
+    public class ClientesUsuariosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClientesUsuariosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /ClientesUsuarios
+        public async Task<IActionResult> Index()
+{
+    var model = new ClientesUsuarios
+    {
+        Clientes = await _context.DataCliente.ToListAsync(),
+        Usuarios = await _context.DataUsuario.ToListAsync()
+    };
+
+    // Renderiza la vista en Views/Admin/ClienteUsuarioCRUD.cshtml
+    return View("~/Views/Admin/ClienteUsuarioCRUD.cshtml", model);
+}
+
+    }
+}
