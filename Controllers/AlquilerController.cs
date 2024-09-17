c1aa96703bc36c8b850fd8e8fbc67dd0f2388592
Jorge Ramirez <jorge_ramirez13@usmp.pe>
2024-09-17 00:08:42 -0500

    AlquilerController configurado

--- Controllers/AlquilerController.cs
+++ Controllers/AlquilerController.cs
@@ -1,26 +1,25 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+using MUSEODESCALZOS.Data;
+using MUSEODESCALZOS.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using MuseoDescalzos.Models;
+
 
 namespace MUSEODESCALZOS.Controllers
 {
-    [Route("[controller]")]
     public class AlquilerController : Controller
     {
-        private readonly ILogger<AlquilerController> _logger;
+        private ApplicationDbContext _context;
 
-        public AlquilerController(ILogger<AlquilerController> logger)
+        public AlquilerController(ApplicationDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var alquiler = await _context.Alquiler.ToListAsync();
+            return View(alquiler);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
@@ -28,5 +27,106 @@ namespace MUSEODESCALZOS.Controllers
         {
             return View("Error!");
         }
+
+        /*Creando funciones*/
+        //Crear
+
+        [HttpGet]
+        public IActionResult CrearAlquiler()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CrearAlquiler(Alquiler alquiler)
+        {
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Add(alquiler);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError(string.Empty, $"Algo salió mal{ex.Message}");
+                }
+            }
+            ModelState.AddModelError(string.Empty, $"Algo salió mal modelo inválido");
+            return View(alquiler);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditarAlquiler(int id)
+        {
+            var exist = await _context.Alquiler.Where(x => x.IdAlquiler == id).FirstOrDefaultAsync();
+            return View(exist);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditarAlquiler(Alquiler alquiler)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var exist = _context.Alquiler.Where(x => x.IdAlquiler == alquiler.IdAlquiler).FirstOrDefault();
+                    if (exist != null)
+                    {
+                        exist.Titulo = alquiler.Titulo;
+                        exist.Descripcion = alquiler.Descripcion;
+                        exist.Capacidad = alquiler.Capacidad;
+                        exist.Caracteristicas = alquiler.Caracteristicas;
+                        exist.Hora_Disponible = alquiler.Hora_Disponible;
+                        exist.Disponible = alquiler.Disponible;
+                    }
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError(string.Empty, $"Algo salió mal{ex.Message}");
+                }
+            }
+            ModelState.AddModelError(string.Empty, $"Algo salió mal modelo inválido");
+            return View(alquiler);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EliminarAlquiler(int id)
+        {
+            var exist = await _context.Alquiler.Where(x => x.IdAlquiler == id).FirstOrDefaultAsync();
+            return View(exist);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EliminarAlquiler(Alquiler alquiler)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var exist = _context.Alquiler.Where(x => x.IdAlquiler == alquiler.IdAlquiler).FirstOrDefault();
+                    if (exist != null)
+                    {
+                        _context.Remove(exist);
+                        await _context.SaveChangesAsync();
+                        return RedirectToAction("Index");
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError(string.Empty, $"Algo salió mal{ex.Message}");
+                }
+            }
+            ModelState.AddModelError(string.Empty, $"Algo salió mal modelo inválido");
+            return View(alquiler);
+        }
+
+        
+
     }
 }
