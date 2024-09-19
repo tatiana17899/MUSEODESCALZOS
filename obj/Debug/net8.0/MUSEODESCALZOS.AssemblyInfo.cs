3725dca05d338199d4dbef8adbe221615de3e7f6
tatiana17899 <tatianasuarezrosas332@gmail.com>
2024-09-19 01:03:54 -0500

    tarea

--- obj/Debug/net8.0/MUSEODESCALZOS.AssemblyInfo.cs
+++ obj/Debug/net8.0/MUSEODESCALZOS.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MUSEODESCALZOS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccd76de83c0fa3342d05c4786a2eba2e41af0ce2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daba058391e19165565e6e3e9e088b6180990cb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MUSEODESCALZOS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MUSEODESCALZOS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
