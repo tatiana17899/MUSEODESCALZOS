dbae5bb27fc154ffb99c73c6b58ec2423a9dacd3
tatiana17899 <tatianasuarezrosas332@gmail.com>
2024-09-21 22:37:24 -0500

    Merge Guia

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -23,8 +23,6 @@ public class ApplicationDbContext : IdentityDbContext
     public DbSet<MuseoDescalzos.Models.PedidoVisita> DataPedidoVisita { get;set; }
     public DbSet<MuseoDescalzos.Models.Usuario > DataUsuario { get;set; }
     public DbSet<MuseoDescalzos.Models.Tarea > DataTareas { get;set; }
-<<<<<<< HEAD
     public DbSet<MuseoDescalzos.Models.Administrador> DataAdministrador { get; set; }
-=======
->>>>>>> guia
+
 }
