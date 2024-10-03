a583d5d72c72fba3efcb818a147fd1f519cee5bc
tatiana17899 <tatianasuarezrosas332@gmail.com>
2024-10-03 14:20:00 -0500

    aaaa

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -35,6 +35,11 @@ namespace MuseoDescalzos.Controllers
             return View();
         }
 
+        public IActionResult Calendar()
+        {
+            return View();
+        }
+        
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
