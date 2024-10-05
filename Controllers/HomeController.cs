96d60742233d3ec4b1d8d015b72f76dbef873b12
tatiana17899 <tatianasuarezrosas332@gmail.com>
2024-10-05 16:54:43 -0500

    diseño

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,19 +1,25 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using MuseoDescalzos.Models;
+using MUSEODESCALZOS.Data;
 using MUSEODESCALZOS.Models;
 namespace MUSEODESCALZOS.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-    public HomeController(ILogger<HomeController> logger)
+    private readonly ApplicationDbContext _context;
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
     
     public IActionResult Index()
     {
-        return View();
+        List<Evento> eventos = _context.DataEvento.ToList(); 
+
+        return View(eventos);
     }
 
     public IActionResult Visita()
