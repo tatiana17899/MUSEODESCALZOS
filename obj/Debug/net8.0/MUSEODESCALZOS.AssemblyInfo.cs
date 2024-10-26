927c0c1bd1eda3bcc8e49e4785fb6b992cd7be58
melivezla <131054190+melivezla@users.noreply.github.com>
2024-10-26 03:34:34 -0500

    aa

--- obj/Debug/net8.0/MUSEODESCALZOS.AssemblyInfo.cs
+++ obj/Debug/net8.0/MUSEODESCALZOS.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MUSEODESCALZOS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3e01e9f9bb654fd52b1664c29fe5ed78c4dca6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c4405c4307906fdce3aceb3edccc18bdd857a42")]
 [assembly: System.Reflection.AssemblyProductAttribute("MUSEODESCALZOS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MUSEODESCALZOS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
