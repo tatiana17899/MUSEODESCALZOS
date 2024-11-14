2b9933f2e808da010d1d3ef040258dd919f299d6
RobinsonHL18 <herreral.jrobinson@gmail.com>
2024-11-14 03:17:46 +0000

    aaaa

--- obj/Debug/net8.0/MUSEODESCALZOS.AssemblyInfo.cs
+++ obj/Debug/net8.0/MUSEODESCALZOS.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MUSEODESCALZOS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26924d5ecdbe5c835472a6739a5f75e6cda42ef6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6159e171c2bb4840ef1d3469d45c659cdbd9d542")]
 [assembly: System.Reflection.AssemblyProductAttribute("MUSEODESCALZOS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MUSEODESCALZOS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
